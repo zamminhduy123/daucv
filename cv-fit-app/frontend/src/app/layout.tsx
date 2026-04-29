186903d395ead42b24f00a5b14803dde6b1c2e08
zamminhduy123 <ntminhduy123@gmail.com>
2026-04-29 10:45:05 +0700

    Add: Google Analytic

--- cv-fit-app/frontend/src/app/layout.tsx
+++ cv-fit-app/frontend/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -39,6 +40,20 @@ export default function RootLayout({
   return (
     <html lang="vi">
       <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-PD8LM96EWZ"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-PD8LM96EWZ');
+          `}
+        </Script>
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
